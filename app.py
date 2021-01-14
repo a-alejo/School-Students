dcbcb1b06be38f64aef1bd0e3bd1a34f3e1f63f7
aalejo92 <76714792+aalejo92@users.noreply.github.com>
2021-01-14 14:07:43 -0500

    Update app.py
    01-09-21 Cleaned the code a bit

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from Student import Student
-#Choices are: name, age, major, gpa, is_graduating_this_year
+# Choices are: name, age, major, gpa, is_graduating_this_year
 
 Student1 = Student("David", 21, 'English', 2.7, True)
 Student2 = Student("Alexa", 19, 'Math', 3.1, False)
 Student3 = Student("Sofia", 24, 'Science', 3.7, True)
 Student4 = Student("Tom", 22, 'Art', 2.9, False)
-# Student_All = Student1, Student2, Student3, Student4
+Student_All = Student1, Student2, Student3, Student4
 
 
-
-
-print(Student1.name, Student1.honor_roll())
+for x in Student_All:
+    print(x.name, x.honor_roll())
